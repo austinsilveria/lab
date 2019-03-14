1a36703466ce4f9743854b8e6e342693060a5b65
austin <austinsilveria@gmail.com>
2019-03-14 11:34:11 -0700

    Spatial outputs setup

--- Networks.py
+++ Networks.py
@@ -7,8 +7,10 @@ class Networks:
         pass
 
     def FullConvBase(self, input):
-        conv1 = layers.Conv2D(16, 5, activation='relu', name='conv1')(input)
-        conv2 = layers.Conv2D(32, 3, activation='relu', name='conv2')(conv1)
+        conv1 = layers.Conv2D(16, 5, input_shape=input.shape, padding='same',
+                              activation='relu', name='conv1')(input)
+        conv2 = layers.Conv2D(32, 3, padding='same',
+                              activation='relu', name='conv2')(conv1)
         return conv2
 
     def SC2FullConv(self):
@@ -16,13 +18,16 @@ class Networks:
         non_spatial_input = Input(shape=(1, 64, 64, 3),
                                   dtype='float64', name='non_spatial')
         screen_input = Input(shape=(1, 64, 64, 17),
-                              dtype='int32', name='screen')
+                             dtype='int32', name='screen')
         minimap_input = Input(shape=(1, 64, 64, 7),
                               dtype='int32', name='minimap')
-        screen_out = self.FullConvBase(screen_input)
-        minimap_out = self.FullConvBase(minimap_input)
+        screen_mid = self.FullConvBase(screen_input)
+        minimap_mid = self.FullConvBase(minimap_input)
         # State representation shape: (1, 64, 64, 27)
-        state_rep = backend.concatenate((screen_out,
-                                        minimap_out,
+        state_rep = backend.concatenate((screen_mid,
+                                        minimap_mid,
                                         non_spatial_input))
+        screen_out = layers.Conv1D(1, 1, padding='same', name='screen')(state_rep)
+        screen2_out = layers.Conv1D(1, 1, padding='same', name='screen2')(state_rep)
+        minimap_out = layers.Conv1D(1, 1, padding='same', name='minimap')(state_rep)
 
