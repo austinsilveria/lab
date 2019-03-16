44410fa49d051513358c12aa14d9c937929f541c
Austin Silveria <austinsilveria@gmail.com>
2019-03-15 20:49:25 -0700

    SC2 Network Completed
    Full model to receive processed PySC2 environment data and output probability distributions over each action_id's and each argument type.

--- TestNetworks.py
+++ TestNetworks.py
@@ -1,5 +1,8 @@
+import keras
+from keras.utils import plot_model
 import Networks as nt
 import numpy as np
+
 
 def test_SC2FullConv():
     non_spatial1 = np.random.rand(32, 64, 64, 3)
@@ -11,7 +14,12 @@ def test_SC2FullConv():
     model = nt.Networks().SC2FullConv()
     model.compile(optimizer='adam',
                   loss='categorical_crossentropy')
-    print('Network output:')
+    output = model.predict([non_spatial1, screen1, minimap1])
+    model.summary()
+    plot_model(model, show_shapes=True, to_file='model.png')
+    print('Screen1 output:', output[0].shape)
+    print('Screen2 output:', output[1].shape)
+    print('Minimap output:', output[2].shape)
 
 
 if __name__ == '__main__':
