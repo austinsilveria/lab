69c0e3cd4599806e1074c682e32ec74a8516727d
Austin Silveria <austinsilveria@gmail.com>
2019-03-17 23:28:15 -0700

    Output list of inputs, process available actions to categorical

--- Preprocessor.py
+++ Preprocessor.py
@@ -8,77 +8,89 @@ from SC2Agent import ZergAgent
 
 
 class Preprocessor:
-    def __init__(self, obs, obs_spec, action_spec):
-        self.obs = obs
+    def __init__(self, obs_spec, action_spec):
         self.obs_spec = obs_spec
         self.action_spec = action_spec
 
-    def full_input(self):
-        reward = self.obs.reward
-        screen = self.obs.observation.feature_screen
-        minimap = self.obs.observation.feature_minimap
-        print('Feature Screen Shape: ', screen.shape)
-        print('Feature Minimap Shape: ', minimap.shape)
-        spatial_stack = np.expand_dims(np.copy(screen).
-                                       reshape((screen.shape[1],
-                                                screen.shape[2],
-                                                screen.shape[0])), axis=0).\
+    def process_avail_actions(self, available_actions):
+        avail = np.zeros(len(self.action_spec[0].functions))
+        avail[available_actions] = 1
+        return avail
+
+    def full_input(self, obs):
+        if obs.last():
+            done = 1
+        else:
+            done = 0
+        reward = obs.reward
+        screen = obs.observation.feature_screen
+        minimap = obs.observation.feature_minimap
+        #print('Feature Screen Shape: ', screen.shape)
+        #print('Feature Minimap Shape: ', minimap.shape)
+        screen_stack = np.expand_dims(np.copy(screen).
+                                      reshape((screen.shape[1],
+                                               screen.shape[2],
+                                               screen.shape[0])), axis=0).\
             astype(np.float32)
         minimap_stack = np.expand_dims(np.copy(minimap).
                                        reshape((minimap.shape[1],
                                                 minimap.shape[2],
                                                 minimap.shape[0])), axis=0).\
             astype(np.float32)
-        print('New screen shape', spatial_stack.shape)
-        print('New Minimap shape: ', minimap_stack.shape)
-        #print('All attr: ', self.obs.observation.__dict__.keys())
+        #print('New screen shape', spatial_stack.shape)
+        #print('New Minimap shape: ', minimap_stack.shape)
+        #print('All attr: ', obs.observation.__dict__.keys())
         spatial_features = ['feature_minimap', 'feature_screen']
         variable_features = ['cargo', 'multi_select', 'build_queue']
         available_actions = ['available_actions']
-        print('Action Spec: ', len(self.action_spec[0].functions))
-        print('Single select:', self.obs_spec[0]['single_select'][1])
+        #print('Action Spec: ', len(self.action_spec[0].functions))
+        #print('Single select:', self.obs_spec[0]['single_select'][1])
         max_no = {'available_actions': len(self.action_spec[0].functions),
                   'cargo': 500, 'multi_select': 500, 'build_queue': 10}
         nonspatial_stack = []
-        for k, v in self.obs.observation.items():
+        for k, v in obs.observation.items():
             if k not in spatial_features + variable_features + available_actions:
                 #v[abs(v) == 0] = 1
                 #print('Log Static Features: ', np.log(v.reshape(-1)))
                 nonspatial_stack = np.concatenate((nonspatial_stack, np.log1p(v.reshape(-1))))
-                print('Non-spatial:', k, ':', v.reshape(-1).shape)
+                #print('Non-spatial:', k, ':', v.reshape(-1).shape)
             elif k in variable_features:
                 #v[abs(v) == 0] = 1
                 #print('Variable value: ', v.reshape(-1))
-                #print('Padding: ', np.zeros(max_no[k] * self.obs_spec[0][
+                #print('Padding: ', np.zeros(max_no[k] * obs_spec[0][
                 # 'single_select'][1] - len(v.reshape(-1))))
                 padded_v = np.concatenate((np.log1p(v.reshape(-1)), np.zeros(
                     max_no[k] * self.obs_spec[0]['single_select'][1] - len(v.reshape(-1)))))
                 nonspatial_stack = np.concatenate((nonspatial_stack, padded_v))
-                print('Variable feature:', k, ':', padded_v.shape)
+                #print('Variable feature:', k, ':', padded_v.shape)
             elif k in available_actions:
                 available_actions_v = [1 if action_id in v else 0 for action_id in
                                              np.arange(max_no['available_actions'])]
                 #print('Available actions:', available_actions_v)
-                print('Length before action cat:', len(nonspatial_stack))
+                #print('Length before action cat:', len(nonspatial_stack))
                 nonspatial_stack = np.concatenate(
                     (nonspatial_stack, available_actions_v))
-                print('Length after action cat:', len(nonspatial_stack))
-        state_shape = [shape for shape in spatial_stack.shape[:3]]
-        print('State shape:', state_shape)
+                #print('Length after action cat:', len(nonspatial_stack))
+        state_shape = [shape for shape in screen_stack.shape[:3]]
+        #print('State shape:', state_shape)
         #print('Nonspatial length before reshape:', len(nonspatial_stack))
         nonspatial_stack = np.reshape(np.concatenate((nonspatial_stack,
                                                      np.zeros(3*state_shape[1] *
                                                               state_shape[2] -
                                                               len(nonspatial_stack))))
                                       , tuple(state_shape + [3])).astype(np.float32)
-        print('Non-spatial after reshape:', nonspatial_stack.shape)
-        print('Processed Input:\n')
-        print('\tReward: ', type(reward))
-        print('\tSpatial Stack: ', spatial_stack.dtype, spatial_stack.shape)
-        print('\tMinimap Stack: ', minimap_stack.dtype, minimap_stack.shape)
-        print('\tNon-Spatial Stack: ', nonspatial_stack.dtype, nonspatial_stack.shape)
-        print('\tNon-Spatial Data:', nonspatial_stack)
-        return reward, spatial_stack, minimap_stack, nonspatial_stack
+        #print('Non-spatial after reshape:', nonspatial_stack.shape)
+        #print('Processed Input:\n')
+        #print('\tReward: ', type(reward))
+        #print('\tSpatial Stack: ', spatial_stack.dtype, spatial_stack.shape)
+        #print('\tMinimap Stack: ', minimap_stack.dtype, minimap_stack.shape)
+        #print('\tNon-Spatial Stack: ', nonspatial_stack.dtype, nonspatial_stack.shape)
+        #print('\tNon-Spatial Data:', nonspatial_stack)
+        #return reward, spatial_stack, minimap_stack, nonspatial_stack
+        return [nonspatial_stack, screen_stack, minimap_stack], reward, done
+
+    def __call__(self, obs):
+        return self.full_input(obs)
 
 
 def main(unused_argv):
@@ -102,7 +114,13 @@ def main(unused_argv):
 
                 timesteps = env.reset()
                 agent.reset()
-                Preprocessor(timesteps[0], env.observation_spec(), env.action_spec()).full_input()
+                preprocessor = Preprocessor(env.observation_spec(), env.action_spec())
+                processed = preprocessor(timesteps[0])
+                print('Non-spatial input:', processed[0][0].shape)
+                print('Screen input:', processed[0][1].shape)
+                print('Minimap input:', processed[0][2].shape)
+                print('Reward:', processed[1])
+                print('Done:', processed[2])
 
                 while True:
                     step_actions = [agent.step(timesteps[0])]
